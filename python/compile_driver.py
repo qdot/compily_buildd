af33b65f03b8a6ac7eb3a067e27b0d03a686222b
Kyle Machulis <kyle@nonpolynomial.com>
2009-09-06 21:55:55 -0700

    Fix to PREFIX_DIR in project initialization Added directory checks for module includes in compile_driver

--- python/compile_driver.py
+++ python/compile_driver.py
@@ -3,15 +3,24 @@
 import os
 import sys
 
-build_sys_path = "../build_sys/python/"
 
-if os.path.isdir(build_sys_path):
-    sys.path.append(build_sys_path)
-else:
-    print "Cannot find build system directory, exiting..."
+# The compile_driver script can go one of 3 places:
+# - The python directory of the compily_buildd repo 
+# - The root of a repo using compily_buildd
+# - In the python directory of a repo using compily_buildd as a submodule
+# So, we check for it in all of those places, using the above list as the search order also 
+build_sys_paths = [".", "../compily_buildd/python/", "../../compily_buildd/python/"]
+
+for p in build_sys_paths:
+    if os.path.isdir(p):
+        sys.path.append(p)
+
+try:
+    from TargetBuilder import TargetBuilder
+except ImportError:
+    print "Cannot find build system module, exiting..."
     sys.exit(1)
 
-from TargetBuilder import TargetBuilder
 
 def main(argv=None):
     if argv is None:
